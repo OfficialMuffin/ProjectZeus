af64332d952032a82abf315b3720117ddc9c9fd3
Leon <39170130+OfficialMuffin@users.noreply.github.com>
2018-05-11 12:08:34 +0100

    pubgWIP.java
    Main Game

--- pubgWIP.java
+++ pubgWIP.java
@@ -9,21 +9,25 @@ public class pubgWIP {
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
 
-        //Game Variables
+        //Game and Enemy Variables
         String[] enemies = { "Enemy with Assault Rifle", "Enemy with SMG", "Enemy with Sniper", "Enemy with Semi Auto Sniper"};
-        int maxEnemyHealth = 100;
-        int assaultAttackDamage = 25;
-        int smgAttackDamage = 15;
-        int sniperAttackDamage = 35;
-        int semiAutoSniperAttackDamage = 45;
+        //Find a way to connect attackDamage with the appropriate enemy type
+        //Integer[] attackDamage = { 25, 15, 35, 45 };
+          int enemyAttackDamage = 25;
+          int maxEnemyHealth = 100;
+//        final int assaultAttackDamage = 25;
+//        final int smgAttackDamage = 15;
+//        final int sniperAttackDamage = 35;
+//        final int semiAutoSniperAttackDamage = 45;
 
         //Player Variables
         int health = 100;
-        int attackDamage = 25;
+        int playerAttackDamage = 25;
         int numHealthBandages = 0;
+        int numMedkits = 0;
         int healAmount = 25;
         int bandageDropChance = 35; //Percentage
-        int medkitDropChance = 10; //Percentage
+        int medkitDropChance = 5; //Percentage
         int medkitHealAmount = 75; //Percentage
 
 
@@ -46,12 +50,13 @@ public class pubgWIP {
                 System.out.println("\n\tWhat would you like to do?");
                 System.out.println("\t1. Attack");
                 System.out.println("\t2. Use Bandage");
-                System.out.println("\t3. Keep Running");
+                System.out.println("\t3. Use Medkit");
+                System.out.println("\t4. Keep Running");
 
                 String input = in.nextLine();
                 if(input.equals("1")) {
-                    int damageDealt = rand.nextInt(attackDamage);
-                    int damageTaken = rand.nextInt(assaultAttackDamage);
+                    int damageDealt = rand.nextInt(playerAttackDamage);
+                    int damageTaken = rand.nextInt(enemyAttackDamage);
 
                     enemyHealth -= damageDealt;
                     health -= damageTaken;
@@ -81,7 +86,24 @@ public class pubgWIP {
                     }
 
                 }
-                else if(input.equals("3")) {
+                else if (input.equals("3")) {
+                    if(numMedkits > 0) {
+                        //Adjust medkit heal amount
+                        health += medkitHealAmount;
+                        numMedkits--;
+                        System.out.println("\tUsing Medkit...");
+                        //Thread.sleep(7500);
+                        System.out.println("You have healed for " + medkitHealAmount + "."
+                                + "\n\t> You now have " + health + " HP."
+                                + "\n\t> You have " + numMedkits + " medkits left.\n");
+                    }
+                    else {
+                        System.out.println("\t> You have no medkits left! Defeat enemies to pick up more!");
+                    }
+
+
+                }
+                else if(input.equals("4")) {
                     System.out.println("You run away from the " + enemy + "!");
                     continue GAME;
                 }
@@ -101,6 +123,11 @@ public class pubgWIP {
                 numHealthBandages++;
                 System.out.println(" # The " + enemy + " had " + numHealthBandages + " bandage(s)! # ");
                 System.out.println(" # You now have " + numHealthBandages + " bandage(s). # ");
+            }
+            if (rand.nextInt(100) > medkitDropChance){
+                numMedkits++;
+                System.out.println(" # The " + enemy + " had " + numMedkits + " medkit(s)! # ");
+                System.out.println(" # You now have " + numMedkits + " medkit(s). # ");
             }
             System.out.println("-------------------------------------------------------------------");
             System.out.println("What would you like to do now?");
@@ -129,3 +156,4 @@ public class pubgWIP {
 
     }
 }
+
